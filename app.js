3df75d06b89b213666bdb1ee14db97f7887d9c3a
Yaretzy Zuail Ramírez Bravo <yaretzy.rb.21@gmail.com>
2025-01-17 15:40:35 -0600

    Add app.js features

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Declaración de variables
+const listaAmigos = [];
+
+//Agregar nombres a la lista de amigos
+function agregarAmigo(){
+    let nombreAmigo = document.getElementById('amigo').value;
+        if(nombreAmigo == ""){
+            alert('Ingrese un nombre válido.')
+        } else{
+            listaAmigos.push(nombreAmigo);
+            imprimirLista();
+        }
+}
+
+//Mostrar la lista de amigos actualizada
+function imprimirLista(){
+    let elementoLista = document.getElementById('listaAmigos');
+    let lista = "";
+    for (nombreAmigo of listaAmigos){
+        lista += `${nombreAmigo} ${(listaAmigos.length == 1) ? '' : "<br>"}`;
+    }
+    elementoLista.innerHTML = lista;
+}
+
+//Botón para sortear amigo
+function sortearAmigo(){
+    let elementoResultado = document.getElementById('resultado');
+    elementoResultado.innerHTML = listaAmigos[Math.floor(Math.random()*listaAmigos.length)+1].toString();
+    }
